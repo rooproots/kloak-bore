e82bc894278b9fc257003a9ff596c2d848736bcb
rooproots <148568669+rooproots@users.noreply.github.com>
2026-03-31 14:52:37 -0500

    Update vite.config.ts to set base path for GitHub Pages deployment

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: '/kloak-bore/',
+});
